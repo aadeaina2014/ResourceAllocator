5f572ef13b302bfd7284157dd82bbfd7e53c0cb5
aadeaina2014 <aadeaina2014@my.fit.edu>
2016-02-18 23:17:33 -0500

    Added files via upload

---
+++ ResourceAllocator.cpp
@@ -0,0 +1,69 @@
+#include "Resourceallocator.h"
+#include <iostream>
+#include <math.h>
+#include <stdexcept>
+
+Resourceallocator::Resourceallocator()
+{
+  
+	for (int i = 0; i < 24; i++)
+	{
+		isrush.push_back(false);
+	}
+// Default values of resource object attributes
+}
+
+bool Resourceallocator::IsRushHour(float t)
+{   
+	try
+	{
+		int index = (int)t;
+		return isrush.at(index); 
+	}
+	catch (std::out_of_range & oor)
+	{
+		std::cerr << "Out of Range error: " << oor.what() << '\n';
+	}
+		
+
+}
+
+void Resourceallocator::AddTimeSpan(float start_time, float stop_time)
+{
+	//If the input is invalid(meaning T<0 or T >= 24.00), behvaiour is not defined.
+
+	int sp = ceil(stop_time);
+	int st = floor(start_time);
+	
+	std::cout << "\nQuery Interval "<< sp << "\t" << st <<"\n\n"<< std::endl;
+	if ((0< st) & (sp <23))
+	{
+		bool status;
+		status = true;
+		float stime =1.0f;
+
+		for (int i = st; i < sp; i++)
+		{
+			if (!isrush[i])
+			{
+				isrush[i] = (status);
+			}
+			else
+			{
+				stime = (float)i;
+				std::cout << "Time Slot " << std::fixed << std::setprecision(2) << stime << "\thas been taken \n" << std::endl;
+				
+			}
+		}
+
+	}
+	else
+	{
+		std::cout << "Invalid Time Input" << std::endl; //check statement
+	}
+}
+
+Resourceallocator::~Resourceallocator()
+{}
+
+
---
+++ main.cpp
@@ -0,0 +1,60 @@
+// main Application Driver File for time resource allocation class
+
+
+#include "Resourceallocator.h"
+#include <iostream>
+using namespace std;
+
+
+
+void queryServer(Resourceallocator *p)
+{
+	std::setprecision(2);
+	bool rushStatus;
+	float timeStamp;
+	cout << "Time Stamp\t" << "\tServer Response:" << endl;
+	cout << "------------------------------------------" << endl;
+	for (int i = 0; i < 24; i++)
+	{
+		rushStatus = p->IsRushHour((float)i);
+		timeStamp = (float)i ;
+		if (rushStatus)
+		{
+			cout << fixed << setprecision(2)<< " "<<timeStamp << " \t\t\tRush Hour" << endl;
+		}
+
+		else
+		{
+			cout << fixed << setprecision(2)<< " "<<timeStamp<< " \t\t\tNot Rushour" << endl;
+
+		}
+
+	}
+
+}
+
+int main()
+{
+	Resourceallocator * timeServer = new Resourceallocator;// Instantiate a pointer to a dynamic resource allocator object
+	cout << "\t**********************************************************************************" << endl;
+	cout << "\t\tNetwork Resource Allocation" << endl;
+	cout << "\t\tDeveloped by Ayokunle Ade-Aina (c) 2016 , All rights reserved" << endl;
+	cout << "\t**********************************************************************************" << endl;
+	timeServer->AddTimeSpan(1.30, 3.30); //allocate time resource on the server
+	system("pause");
+
+	queryServer(timeServer);  // Query server for all available time slots
+	system("pause");
+	timeServer->AddTimeSpan(1.30, 3.30); //simulate duplicate timespan allocation
+	system("pause");
+	queryServer(timeServer);  // Query server for all available time slots
+	system("pause");
+	timeServer->AddTimeSpan(11.30, 17.30); 
+	system("pause");
+	queryServer(timeServer);  // Query server for all available time slots
+	delete timeServer;
+	system("pause");
+
+	
+	return EXIT_SUCCESS;
+}
